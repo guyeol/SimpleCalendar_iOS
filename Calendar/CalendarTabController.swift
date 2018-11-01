90f29ff6e0e9321a4c51b840848f58e42e911b88
정구열 <jeong-guyeol@jeong-guyeol-ui-MacBook-2.local>
2018-11-01 21:27:18 +0900

    Create CalendarTabController.swift

--- Calendar/CalendarTabController.swift
+++ Calendar/CalendarTabController.swift
@@ -1,28 +1,24 @@
 //
-//  ViewController.swift
+//  CalendarViewController.swift
 //  Calendar
 //
-//  Created by 정구열 on 30/10/2018.
+//  Created by 정구열 on 01/11/2018.
 //  Copyright © 2018 guyeol_jeong. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class CalendarTabController: UITabBarController{
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    let mainTabController = storyboard?.instantiateViewController(withIdentifier: "CalendarTabController") as! CalendarTabController
-    present(mainTabController, animated: true, completion: nil)
-    
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  
 }
-
