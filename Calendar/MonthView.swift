913b1b54afd4676df5cfc214d6331a7a10b335d7
정구열 <jeong-guyeol@jeong-guyeol-ui-MacBook-2.local>
2018-11-02 12:12:01 +0900

    MonthView file added

--- Calendar/MonthView.swift
+++ Calendar/MonthView.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import UIKit
+
+protocol MonthViewDelegate: class {
+  func didChangeMonth(MonthIndex: Int, year: Int)
+}
+
+class MonthView: UIView {
+  var MonthArr = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
+  var currentMonthIdx=0
+  var currentYear=0
+  var delegate: MonthViewDelegate?
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.backgroundColor=UIColor.clear
+    currentMonthIdx=Calendar.current.component(.month, from: Date())
+    
+  }
+}
