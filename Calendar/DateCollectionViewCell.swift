176ea0411e5637f0332c69d42af478e32eba207d
정구열 <jeong-guyeol@jeong-guyeol-ui-MacBook-2.local>
2018-11-03 03:19:31 +0900

    feat: DateCollectionViewCell, cell 모양 원형으로 변경

--- Calendar/DateCollectionViewCell.swift
+++ Calendar/DateCollectionViewCell.swift
@@ -10,4 +10,14 @@ import UIKit
 
 class DateCollectionViewCell: UICollectionViewCell {
     
+  @IBOutlet weak var DateLabel: UILabel!
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    self.setCircularImageView()
+  }
+  
+  func setCircularImageView() {
+    self.DateLabel.layer.cornerRadius = self.DateLabel.frame.width/2
+  }
 }
